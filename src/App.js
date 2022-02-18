688a44fc309132f83648cfa8cff3d55f04bbe0ad
Mohammad Alghrawe <mohammad.alghrawe@gmail.com>
2022-02-18 14:02:26 +0100

    created the components of the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import Candy from "./components/candy/candy";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello!!</h1>
-      </header>
+      <Candy />
     </div>
   );
 }
---
+++ src/components/candy/candy.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Candy = () => {
+  return (
+    <div>
+      <h3>Candy</h3>
+    </div>
+  );
+};
